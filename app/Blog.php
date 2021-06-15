b84e21dcbc039613e639136c8a1df14eb5eaa4ae
Rasul Kadraliev <rasul.kadraliev@gmail.com>
2021-06-15 15:10:13 -0700

    Commit Blog project files

---
+++ app/Blog.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Blog extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'blog';
+}
+{
+    //
+}
---
+++ database/migrations/2021_06_15_185158_create_blog_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBlogTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blog', function (Blueprint $table) {
+            $table->id();
+            $table->string('header');
+            $table->text('text');
+            $table->string('author');
+            $table->boolean('published');
+            $table->date('date_published');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blog');
+    }
+}
